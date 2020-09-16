ca2dc1362cfe9a7d6bc2971552686a63b013b44d
newfolk1 <37738112+ixap2i@users.noreply.github.com>
2020-09-16 19:28:06 +0900

    add html component

--- Code.js
+++ Code.js
@@ -1,3 +1,8 @@
-function myFunction() {
-  
+function doGet() {
+  try {
+    return HtmlService.createHtmlOutputFromFile('sales-list')
+    .setSandboxMode(HtmlService.SandboxMode.IFRAME);  
+  } catch(e) {
+    Logger.log(e);
+  }
 }
